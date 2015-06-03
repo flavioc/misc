507f6410032330bcf61bcc5b4469bb345fe2b08b
Flavio Cruz <flaviocruz@gmail.com>
2015-06-03 02:52:02 +0100

    Add forgotten mem.hpp and .gitignore.

---
+++ mem.hpp
@@ -0,0 +1,77 @@
+#ifndef MEM_HPP
+#define MEM_HPP
+
+//#define MEASURE_MEM
+
+#include <stdio.h>
+#include <string.h>
+
+ int parseLine(char* line)
+{
+    int i = strlen(line);
+    while (*line < '0' || *line > '9') line++;
+    line[i-3] = '\0';
+    i = atoi(line);
+    return i;
+ }
+
+int getMemUsage()
+{
+   // memory usage in KB.
+   FILE* file = fopen("/proc/self/status", "r");
+   if(!file)
+      return -1;
+
+   int result = -1;
+   char line[128];
+
+   while (fgets(line, 128, file) != NULL){
+      if (strncmp(line, "VmSize:", 6) == 0){
+         result = parseLine(line);
+         break;
+      }
+   }
+   fclose(file);
+   return result;
+ }
+
+#ifdef MEASURE_MEM
+static uint64_t start_mem = 0;
+static uint64_t avg_memory = 0;
+static uint64_t mem_count = 0;
+#endif
+
+static void
+initMemory()
+{
+#ifdef MEASURE_MEM
+   start_mem = getMemUsage();
+#endif
+}
+
+static void
+readMemory()
+{
+#ifdef MEASURE_MEM
+   mem_count++;
+   if(mem_count == 1) {
+      avg_memory = getMemUsage() - start_mem;
+   } else {
+      double frac(((double)(mem_count-1)) / ((double)mem_count));
+      double other(1.0 - frac);
+      avg_memory = frac * ((double)avg_memory) + other * ((double)(getMemUsage() - start_mem));
+   }
+#endif
+}
+
+static void
+printMemory()
+{
+#ifdef MEASURE_MEM
+   if(!avg_memory)
+      avg_memory = 1;
+   std::cout << avg_memory << std::endl;
+#endif
+}
+
+#endif
